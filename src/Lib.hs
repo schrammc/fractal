e50eed330bdc6944dea4a77bcf64d5cc984799e2
Christof Schramm <christof.schramm@gmx.de>
2019-05-14 16:53:57 +0200

    Add some comments to the code

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,21 +11,30 @@ import           Graphics.Rendering.Chart.Backend.Cairo
 import           Graphics.Rendering.Chart.Easy
 import           Linear
 
-prepost :: V2 Double -> V2 Double
-prepost a = (1/3) *^ a
+-- | Cut a vector to a third of it's length
+thirdlen :: V2 Double -> V2 Double
+thirdlen a = (1/3) *^ a
 
+-- | For a given vector segments give two vectors that, when added result in the
+-- original vector. These vectors will be rotated by 60 degrees to form an
+-- equilateral triangle with the original vector.
 insertRand :: MonadRandom m => V2 Double -> m (V2 Double,V2 Double)
 insertRand a = do
   invert <- getRandom
   if invert
-    then return $ (rotateV2 (pi/3) (prepost a), rotateV2 (5*(pi/3)) (prepost a))
-    else return $ (rotateV2 (5*(pi/3)) (prepost a), rotateV2 (pi/3) (prepost a))
+    then return $ ( rotateV2 (pi/3) (thirdlen a)
+                  , rotateV2 (5*(pi/3)) (thirdlen a))
+    else return $ ( rotateV2 (5*(pi/3)) (thirdlen a)
+                  , rotateV2 (pi/3) (thirdlen a))
 
+-- | Rotate a given two dimensional vector by the given angle (polar
+-- coordinates)
 rotateV2 :: Double -> V2 Double -> V2 Double
 rotateV2 angle v = mat !* v
   where
     mat = V2 (V2 (cos angle) ((-1) * sin angle)) (V2 (sin angle) (cos angle))
 
+-- | Perform one randomized initiator-generator cycle of fractal generation.
 step :: MonadRandom m => VU.Vector (V2 Double) -> m (VU.Vector (V2 Double))
 step v = evalStateT (VU.generateM n' build) Nothing
   where
@@ -33,7 +42,7 @@ step v = evalStateT (VU.generateM n' build) Nothing
     n' = 4 * n
     build i =
       if | i `mod` 4 == 0 || i `mod` 4 == 3 -> do
-             return $ (prepost $ (VU.!) v (i `div` 4))
+             return $ (thirdlen $ (VU.!) v (i `div` 4))
          | i `mod` 4 == 1 -> do
              (a,b) <- lift $ insertRand $ (VU.!) v (i `div` 4)
              put $ Just b
@@ -45,6 +54,7 @@ step v = evalStateT (VU.generateM n' build) Nothing
                Just x -> put Nothing >> return x
          | otherwise -> error "Impossible case (2)"
 
+-- | Perform a number of (randomized) initiator generator cycles.
 steps :: MonadRandom m => Int -> m [(VU.Vector (V2 Double))]
 steps n
   | otherwise = fmap reverse $ go n $ VU.fromList [V2 1 0]
@@ -56,6 +66,7 @@ steps n
         rest <- go (i - 1) this
         return $ this : rest
 
+-- | Generate a nice pdf-rendered graphic
 chartSteps :: Int -> IO ()
 chartSteps n = do
   sts <- evalRandIO $ steps n
@@ -84,5 +95,7 @@ regularN n
     let rotationAngle = 2*pi*(k/fromIntegral n)
     return $ rotateV2 rotationAngle (V2 1 0)
 
+-- | Turn a set of affine vectors into a path (list of points) by cumulatively
+-- adding them up.
 toDisplay :: VU.Vector (V2 Double) -> [(Double, Double)]
 toDisplay v = fmap (\(V2 a b) -> (a,b)) $ VU.toList $ VU.scanl1 (+) v 
